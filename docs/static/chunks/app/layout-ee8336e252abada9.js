31c0f4cdc4f892d64904ce7628774db0a7f9ab11
TEOH XI XIAN <25006498@siswa.um.edu.my>
2025-12-19 22:44:07 +0800

    Add docs folder for GitHub Pages deployment

---
+++ docs/static/chunks/app/layout-ee8336e252abada9.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{3371:function(e,r,n){Promise.resolve().then(n.bind(n,9604)),Promise.resolve().then(n.t.bind(n,2853,23))},2853:function(){},622:function(e,r,n){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var t=n(2265),o=Symbol.for("react.element"),f=Symbol.for("react.fragment"),s=Object.prototype.hasOwnProperty,_=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,u={key:!0,ref:!0,__self:!0,__source:!0};function q(e,r,n){var t,f={},i=null,c=null;for(t in void 0!==n&&(i=""+n),void 0!==r.key&&(i=""+r.key),void 0!==r.ref&&(c=r.ref),r)s.call(r,t)&&!u.hasOwnProperty(t)&&(f[t]=r[t]);if(e&&e.defaultProps)for(t in r=e.defaultProps)void 0===f[t]&&(f[t]=r[t]);return{$$typeof:o,type:e,key:i,ref:c,props:f,_owner:_.current}}r.Fragment=f,r.jsx=q,r.jsxs=q},7437:function(e,r,n){"use strict";e.exports=n(622)}},function(e){e.O(0,[604,971,472,744],function(){return e(e.s=3371)}),_N_E=e.O()}]);
